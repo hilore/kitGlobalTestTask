d0d2851125f3e3038631d615d4163b57ffa7f67f
Mark Parker <marksadpepe@gmail.com>
2024-06-24 04:02:10 +0300

    removed userId field from craete task DTO class

--- src/task/dto/create.dto.ts
+++ src/task/dto/create.dto.ts
@@ -1,10 +1,6 @@
 import { IsString, Length } from "class-validator";
 
 export class CreateTaskDto {
-  @IsString({message: "User ID must be a string"})
-  @Length(24, 24, {message: "User ID must be exactly 24 characters long"})
-  userId: string;
-
   @IsString()
   @Length(10, 20)
   title: string;
