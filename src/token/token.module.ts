99dae2b11cc718e0a5b728a0cd93bd4bc7239f6b
Mark Parker <marksadpepe@gmail.com>
2024-06-25 03:14:10 +0300

    replaced process.env variables with Config module and service

--- src/token/token.module.ts
+++ src/token/token.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TokenService } from './token.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Token, TokenSchema } from './schemas/token.schema';
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
   imports: [
+    ConfigModule,
     MongooseModule.forFeature([{ name: Token.name, schema: TokenSchema }]),
   ],
   providers: [TokenService],
