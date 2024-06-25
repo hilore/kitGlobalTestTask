3cad323b8599d6d1a354bbb977fcab9072c56012
Mark Parker <marksadpepe@gmail.com>
2024-06-25 08:22:12 +0300

    added ConfigModule import

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,11 +5,13 @@ import { TokenModule } from '../token/token.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
 import {AuthGuard} from "./auth.guard";
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     TokenModule,
+    ConfigModule
   ],
   providers: [AuthService, AuthGuard],
   controllers: [AuthController],
